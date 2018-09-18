4750b689e11accc6272a75f4cf825455a4b8a839
Kim Pham <pham.kim.a@gmail.com>
2018-09-17 22:22:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    # create a controller action responds to a GET request and loads index.erb in the browser
+    get '/' do
+        erb :index
+    end
 
 end
